dd3dd6fb7a37f17433188f016b7a288e11aabc81
Jesse Wolgamott <jesse@comalproductions.com>
2017-06-27 18:11:05 -0500

    PORT not port

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post("/speak", function(req, res) {
   });
 })
 
-const port = (process.env.port || 3000)
+const port = (process.env.PORT || 3000)
 app.listen(port, function(){
   console.log(`OK, listening on ${port}`)
 })
