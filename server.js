16d1a4abd00a6709f95d5085a1977493735e1240
Jesse Wolgamott <jesse@comalproductions.com>
2017-06-27 17:38:26 -0500

    Node with polly

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const facts = require("./src/facts");
+const AWS = require('aws-sdk');
+const path = require("path");
+require('dotenv').config()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+const polly = new AWS.Polly({apiVersion: '2016-06-10', region: 'us-east-1', credentials: {
+  accessKeyId: process.env.AWS_SECRET_ID,
+  secretAccessKey: process.env.AWS_ACCESS_KEY
+}});
+
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname+'/index.html'))
+})
+
+app.post("/speak", function(req, res) {
+  const fact = facts.randomFact();
+
+  const params = {
+    OutputFormat: "mp3",
+    Text: fact,
+    TextType: "text",
+    VoiceId: "Joanna"
+  };
+  polly.synthesizeSpeech(params, function(err, data) {
+    if (err) {
+      res.status(422).json(err);
+    } else {
+      res.json(data)
+    }
+  });
+})
+
+const port = (process.env.port || 3000)
+app.listen(port, function(){
+  console.log(`OK, listening on ${port}`)
+})
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const audioElement = document.querySelector("audio")
+
+const handlePlay = function(event){
+  event.preventDefault();
+
+  fetch("/speak", {method: "POST"})
+  .then( (r) => r.json() )
+  .then( (json) => {
+    var uInt8Array = new Uint8Array(json.AudioStream.data);
+    var arrayBuffer = uInt8Array.buffer;
+    var blob = new Blob([arrayBuffer]);
+    var url = URL.createObjectURL(blob);
+    audioElement.src = url;
+    audioElement.play();
+  })
+
+}
+
+document.querySelector("[data-role='play']").addEventListener("click", handlePlay);
